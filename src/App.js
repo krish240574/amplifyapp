6597bf34f9feec3c650d44833f882a27a02745cd
Kumar <kumar.ramanathan@gmail.com>
2022-07-09 21:49:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import ReactTable from "react-table"; 
-import 'react-table/react-table.css'
+//import 'react-table/react-table.css'
 import Select from "react-select";
 
 
